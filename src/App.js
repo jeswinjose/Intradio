000d7daf6dbcf35f909d493e309594ffcbe62c08
Jeswin Jose <jeswinjose@microsoft.com`>
2022-04-23 20:59:49 +0530

    added yml changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getJson = ()=> {
+    fetch("/getInfo").then((res)=>{
+      console.log(res);
+    }, 
+    (rej)=>{
+      console.log('rejected'+rej);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={getJson()}>send request</button>
+     
     </div>
   );
 }
